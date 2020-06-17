8cdb1e7f42cdfa5fc69f2fae6eeed486df9b3050
zzzzbw <zzzzbw@gmail.com>
2020-06-17 23:18:10 +0800

    Init repo

---
+++ src/main/java/cn/zzzzbw/pi/helper/PiHelperApplication.java
@@ -0,0 +1,13 @@
+package cn.zzzzbw.pi.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/zzzzbw/pi/helper/PiHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zzzzbw.pi.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
